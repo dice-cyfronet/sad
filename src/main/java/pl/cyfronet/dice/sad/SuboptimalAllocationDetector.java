0a9683f041ddf78dfcc79baddd78337e0038886d
tbartynski <tomek.bartynski@gmail.com>
2015-02-04 13:22:23 +0100

    Initial commit for app main class

---
+++ src/main/java/pl/cyfronet/dice/sad/SuboptimalAllocationDetector.java
@@ -0,0 +1,12 @@
+package pl.cyfronet.dice.sad;
+
+/**
+ * Created by tomek on 04.02.15.
+ */
+public class SuboptimalAllocationDetector {
+
+    public static void main(String[] args) {
+        System.out.println("Starting Suboptimal Allocation Detector");
+    }
+
+}
