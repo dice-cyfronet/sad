f7cc5d5f9fa5f56fb5e4315658780f2b8fa07752
tbartynski <tomek.bartynski@gmail.com>
2015-02-04 19:08:24 +0100

    Prototyping main app class

--- src/main/java/pl/cyfronet/dice/sad/SuboptimalAllocationDetector.java
+++ src/main/java/pl/cyfronet/dice/sad/SuboptimalAllocationDetector.java
@@ -1,12 +1,52 @@
 package pl.cyfronet.dice.sad;
 
+import com.espertech.esper.client.UpdateListener;
+import pl.cyfronet.dice.sad.cep.Engine;
+import pl.cyfronet.dice.sad.cep.GenericComplexEventListener;
+
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by tomek on 04.02.15.
  */
 public class SuboptimalAllocationDetector {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException, URISyntaxException {
         System.out.println("Starting Suboptimal Allocation Detector");
+        testLoadEvent();
+
+    }
+
+    private static void testLoadEvent() throws InterruptedException, URISyntaxException {
+        Engine engine = new Engine();
+        Map<String, Object> eventDef = new HashMap<String, Object>();
+        eventDef.put("vmUuid", String.class);
+        eventDef.put("cpuLoad", float.class);
+        engine.addEventType("CpuLoad1", eventDef);
+        UpdateListener listener = new GenericComplexEventListener(Configurator.INSTANCE.getProperty("redisUri"));
+        String complexEvDef = "select avg(cpuLoad), vmUuid from CpuLoad1.win:time(5 sec) having avg(cpuLoad) > 0.8 output first every 10 seconds";
+        engine.subscribe(complexEvDef, listener);
+        Map eventMap = null;
+        for(int i=0; i < 20; i++) {
+            eventMap = new HashMap();
+            eventMap.put("cpuLoad", i * 0.2);
+            eventMap.put("vmUuid", "1");
+            engine.sendEvent(eventMap, "CpuLoad1");
+            System.out.println("Sent event for 1 " + i * 0.2);
+            System.out.println("--------------------");
+            Thread.sleep(900);
+        }
+        engine.unsubscribe(complexEvDef);
+        engine.removeEventType("CpuLoad1");
+        System.out.println("Finished!");
+
+    }
+
+    class SimpleEventReceiver {
+
     }
 
 }
