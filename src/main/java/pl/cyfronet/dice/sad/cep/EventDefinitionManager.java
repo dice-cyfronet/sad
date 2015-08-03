a5c41c1f8a90beadb918127468172a20e800fd90
tomek <tomek.bartynski@gmail.com>
2015-08-03 21:28:45 +0200

    first prototype of a ev def manager
    First prototype of a class that enables to manage (add and remove) event definitions.

---
+++ src/main/java/pl/cyfronet/dice/sad/cep/EventDefinitionManager.java
@@ -0,0 +1,45 @@
+package pl.cyfronet.dice.sad.cep;
+
+import pl.cyfronet.dice.sad.Configurator;
+import pl.cyfronet.dice.sad.SADException;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPubSub;
+import redis.clients.jedis.JedisShardInfo;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Created by tomek on 8/3/15.
+ */
+public class EventDefinitionManager {
+    private Engine engine;
+    private Jedis jedis;
+    private String evDefChannel;
+    private JedisPubSub evDefSub;
+
+    public EventDefinitionManager(Engine engine) throws SADException {
+        this.engine = engine;
+        Configurator configurator = Configurator.INSTANCE;
+        String redisURI = configurator.getProperty("redisURI");
+        URI uri;
+        try {
+            uri = new URI(redisURI);
+        } catch (URISyntaxException e) {
+            String exMsg = "Failed to create EventDefinitionManager"
+                    + " due to invalid Redis URI ("
+                    + redisURI + "). Make sure redisURI is defined in props file";
+            throw new SADException(exMsg, e);
+        }
+        JedisShardInfo info = new JedisShardInfo(uri);
+        info.setTimeout(30000);
+        jedis = new Jedis(info);
+        evDefChannel = configurator.getProperty("eventDefinitionChannel");
+        evDefSub = new EventDefinitionSub(engine);
+    }
+
+    public void start() {
+        jedis.subscribe(evDefSub, evDefChannel);
+    }
+
+}
