383b77d060313e1696dd79d23748fa867977fb90
tomek <tomek.bartynski@gmail.com>
2015-08-03 21:23:24 +0200

    added application exception class

---
+++ src/main/java/pl/cyfronet/dice/sad/SADException.java
@@ -0,0 +1,12 @@
+package pl.cyfronet.dice.sad;
+
+import java.net.URISyntaxException;
+
+/**
+ * Created by tomek on 8/3/15.
+ */
+public class SADException extends Exception {
+    public SADException(String exMsg, Throwable cause) {
+        super(exMsg, cause);
+    }
+}
