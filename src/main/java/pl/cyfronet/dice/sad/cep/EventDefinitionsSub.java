f8067582e15b561812986383336f964a90f60afb
tomek <tomek.bartynski@gmail.com>
2015-08-04 15:06:36 +0200

    use logger and switched to json-schema-validator
    sysoutprintln replaced with logger usages.
    
    json simple replaced with json-schema-validator which enables to validate msg accross provided schema.

--- src/main/java/pl/cyfronet/dice/sad/cep/EventDefinitionsSub.java
+++ src/main/java/pl/cyfronet/dice/sad/cep/EventDefinitionsSub.java
@@ -1,11 +1,19 @@
 package pl.cyfronet.dice.sad.cep;
 
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jackson.JsonLoader;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.core.report.ProcessingReport;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.cyfronet.dice.sad.SADException;
 import redis.clients.jedis.JedisPubSub;
 
 
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -13,22 +21,30 @@ import java.util.Map;
  */
 public class EventDefinitionsSub extends JedisPubSub {
 
-    private Engine engine;
-    private JSONParser parser;
+    private static final Logger log = LoggerFactory.getLogger(EventDefinitionsSub.class);
 
-    EventDefinitionsSub(Engine engine) {
+    private final Engine engine;
+
+    private final JsonNode msgSchema;
+    private final JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
+    private final JsonSchema schema;
+    private final ObjectMapper mapper;
+
+    EventDefinitionsSub(Engine engine) throws IOException, ProcessingException {
         this.engine = engine;
-        parser = new JSONParser();
+        msgSchema = JsonLoader.fromResource("/event_defs_schema.json");
+        schema = factory.getJsonSchema(msgSchema);
+        mapper = new ObjectMapper();
     }
 
     @Override
     public void onUnsubscribe(String channel, int subscribedChannels) {
-        System.out.println("onUnsubscribe");
+        log.info("onUnsubscribe");
     }
 
     @Override
     public void onSubscribe(String channel, int subscribedChannels) {
-        System.out.println("onSubscribe");
+        log.info("onSubscribe");
     }
 
     @Override
@@ -46,35 +62,41 @@ public class EventDefinitionsSub extends JedisPubSub {
     @Override
     public void onMessage(String channel, String message) {
         //TODO parse event definition and add event type
-        System.out.println("Message: " + message);
-        Map<String, Object> evDefs = decodeMsgToEvenetDef(message);
+        log.info("Message: " + message);
+        Map<String, Object> evDefs = null;
         try {
-            validateEventDefinitions(evDefs);
+            evDefs = decodeMsgToEventDefs(message);
         } catch (SADException e) {
             e.printStackTrace();
         }
         if (evDefs == null) { return; }
         for (Map.Entry<String, Object> e : evDefs.entrySet()) {
-            System.out.println("Key " + e.getKey() + ", value " + e.getValue());
+            log.info("Key " + e.getKey() + ", value " + e.getValue());
         }
     }
 
-    private boolean validateEventDefinitions(Map<String, Object> evDefs) throws SADException {
-        // TODO not empty, contains a valid simple event definition
-        if (evDefs.isEmpty()) {
-            System.out.println("Provided event definition is empty");
-            return false;
-        }
-        return true;
-    }
-
-    private Map<String, Object> decodeMsgToEvenetDef(String msg) {
+    private Map<String, Object> decodeMsgToEventDefs(String msg) throws SADException {
         try {
-            return (Map) parser.parse(msg);
-        } catch (ParseException e) {
-            e.printStackTrace();
+            JsonNode json = JsonLoader.fromString(msg);
+            ProcessingReport report = schema.validate(json);
+            if (report.isSuccess()) {
+                return mapper.convertValue(json, Map.class);
+            } else {
+                throw new SADException(
+                        String.format("Provided JSON message: %s is not valid", msg)
+                );
+            }
+        } catch (IOException ioe) {
+            throw new SADException(
+                    String.format("Error while parsing JSON message: %s to Map", msg),
+                    ioe
+            );
+        } catch (ProcessingException pe) {
+            throw new SADException(
+                    String.format("Error while validating JSON message: %s with schema: %s", msg, schema),
+                    pe
+            );
         }
-        return null;
     }
 
 }
