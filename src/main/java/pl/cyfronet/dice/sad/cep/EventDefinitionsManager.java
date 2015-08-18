51b859c81ea963c88b42fbb440078f31fc97f61e
tomek <tomek.bartynski@gmail.com>
2015-08-18 16:30:52 +0200

    implemented proper jedis connections management
    JedisPool is used and PubSub object subscribe and unsubcribe to Jedis in a controlled manner.

--- src/main/java/pl/cyfronet/dice/sad/cep/EventDefinitionsManager.java
+++ src/main/java/pl/cyfronet/dice/sad/cep/EventDefinitionsManager.java
@@ -1,52 +1,87 @@
 package pl.cyfronet.dice.sad.cep;
 
-import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.cyfronet.dice.sad.Configurator;
 import pl.cyfronet.dice.sad.SADException;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPubSub;
-import redis.clients.jedis.JedisShardInfo;
+import redis.clients.jedis.*;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.concurrent.Phaser;
 
 /**
  * Created by tomek on 8/3/15.
  */
 public class EventDefinitionsManager {
-    private Engine engine;
-    private Jedis jedis;
+
+    private static final Logger log = LoggerFactory.getLogger(EventDefinitionsManager.class);
+
+    private JedisPool jedisPool;
     private String evDefChannel;
     private String simpleEventChannel;
     private JedisPubSub evDefSub;
     private SimpleEventSub simpleEventSub;
 
-    public EventDefinitionsManager(Engine engine) throws SADException {
-        this.engine = engine;
+    private final Phaser phaser = new Phaser();
+
+    public EventDefinitionsManager() throws SADException {
         Configurator configurator = Configurator.INSTANCE;
-        String redisURI = configurator.getProperty("redisURI");
-        URI uri;
-        try {
-            uri = new URI(redisURI);
-        } catch (URISyntaxException e) {
-            String exMsg = "Failed to create EventDefinitionsManager"
-                    + " due to invalid Redis URI ("
-                    + redisURI + "). Make sure redisURI is defined in props file";
-            throw new SADException(exMsg, e);
-        }
-        JedisShardInfo info = new JedisShardInfo(uri);
-        info.setTimeout(30000);
-        jedis = new Jedis(info);
         evDefChannel = configurator.getProperty("eventDefinitionChannel");
-        evDefSub = EventDefinitionsSub.getInstance(engine);
+        evDefSub = EventDefinitionsSub.getInstance();
         simpleEventChannel = configurator.getProperty("simpleEventChannel");
-        simpleEventSub = new SimpleEventSub(engine);
+        simpleEventSub = new SimpleEventSub();
     }
 
     public void start() {
-        jedis.subscribe(evDefSub, evDefChannel);
-        jedis.subscribe(simpleEventSub, simpleEventChannel);
+        setupJedisPool();
+        setupEvDefinitionsListener();
+        setupSimpleEvListener();
+    }
+
+    private void setupSimpleEvListener() {
+        new Thread(() -> {
+            phaser.register();
+            Jedis jedis = jedisPool.getResource();
+            jedis.subscribe(simpleEventSub, simpleEventChannel);
+            log.debug("Releasing Jedis resource from SimpleEventListener");
+            jedisPool.returnResource(jedis);
+            phaser.arrive();
+        }).start();
+        log.info("Subscribed for simple events");
+    }
+
+    private void setupEvDefinitionsListener() {
+        new Thread(() -> {
+            phaser.register();
+            Jedis jedis = jedisPool.getResource();
+            jedis.subscribe(evDefSub, evDefChannel);
+            log.debug("Releasing Jedis resource from EventDefinitionListener");
+            jedisPool.returnResource(jedis);
+            phaser.arrive();
+        }).start();
+        log.info("Subscribed for event definitions");
+    }
+
+    private void setupJedisPool() {
+        Configurator configurator = Configurator.INSTANCE;
+        String redisHost = configurator.getProperty("redisHost");
+        int redisPort = Integer.parseInt(configurator.getProperty("redisPort"));
+        int redisTimeout = Integer.parseInt(configurator.getProperty("redisTimeout"));
+        String redisPassword = configurator.getProperty("redisPassword");
+        jedisPool = new JedisPool(new JedisPoolConfig(), redisHost, redisPort, redisTimeout, redisPassword);
+        Runtime.getRuntime().addShutdownHook(
+                new Thread(
+                        () -> {
+                            phaser.register();
+                            evDefSub.unsubscribe();
+                            simpleEventSub.unsubscribe();
+                            phaser.arriveAndAwaitAdvance();
+                            if (jedisPool != null) {
+                                jedisPool.destroy();
+                                log.debug("Destroyed Jedis pool");
+                            }
+                        }
+                )
+        );
     }
 
 }
