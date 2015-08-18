26739c1fd99926eb9b6aa2a6902db269b157bd17
tomek <tomek.bartynski@gmail.com>
2015-08-18 23:50:29 +0200

    added a method to create singleton instance

--- src/main/java/pl/cyfronet/dice/sad/cep/RedisComplexEventSink.java
+++ src/main/java/pl/cyfronet/dice/sad/cep/RedisComplexEventSink.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import pl.cyfronet.dice.sad.Configurator;
 import pl.cyfronet.dice.sad.SADException;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisShardInfo;
 
 import java.net.URI;
@@ -19,45 +20,39 @@ public class RedisComplexEventSink implements UpdateListener {
 
     private static volatile RedisComplexEventSink instance;
 
-    public static RedisComplexEventSink getInstance() throws SADException {
+    private JedisPool jedisPool;
+
+    public static void createInstance(JedisPool pool) {
         if (instance  == null) {
             synchronized (RedisComplexEventSink.class) {
                 if (instance == null) {
-                    instance = new RedisComplexEventSink();
+                    instance = new RedisComplexEventSink(pool);
                 }
             }
         }
+    }
+
+    public static RedisComplexEventSink getInstance() {
         return instance;
     }
 
     private static final Logger log = LoggerFactory.getLogger(RedisComplexEventSink.class);
-    private Jedis jedis;
 
-    private RedisComplexEventSink() throws SADException {
-        Configurator configurator = Configurator.INSTANCE;
-        String redisURI = configurator.getProperty("redisURI");
-        URI uri;
-        try {
-            uri = new URI(redisURI);
-        } catch (URISyntaxException e) {
-            String exMsg = "Failed to create RedisComplexEventSink"
-                    + " due to invalid Redis URI ("
-                    + redisURI + "). Make sure it is defined in props file.";
-            throw new SADException(exMsg, e);
-        }
-        JedisShardInfo info = new JedisShardInfo(uri);
-        info.setTimeout(30000);
-        jedis = new Jedis(info);
+    private RedisComplexEventSink(JedisPool pool) {
+        jedisPool = pool;
     }
 
     @Override
     public void update(EventBean[] newEvents, EventBean[] oldEvents) {
+        Jedis jedis = jedisPool.getResource();
         StringBuffer eventStrBuff = new StringBuffer("{\n");
         for (String propName : newEvents[0].getEventType().getPropertyNames()) {
             eventStrBuff.append(propName).append(": ").append(newEvents[0].get(propName)).append("\n");
         }
         String msg = eventStrBuff.append("}").toString();
         log.info("Complex event: " + msg);
+
         jedis.publish("AtmosphereComplexEvent", msg);
+        jedisPool.returnResource(jedis);
     }
 }
