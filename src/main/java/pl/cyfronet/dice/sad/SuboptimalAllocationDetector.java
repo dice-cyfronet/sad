dd3900a5e301d0bdefc199f5675e1c3b0c963f43
tomek <tomek.bartynski@gmail.com>
2015-08-18 22:32:22 +0200

    moved Jedis and listener setup methods to SAD class

--- src/main/java/pl/cyfronet/dice/sad/SuboptimalAllocationDetector.java
+++ src/main/java/pl/cyfronet/dice/sad/SuboptimalAllocationDetector.java
@@ -3,13 +3,16 @@ package pl.cyfronet.dice.sad;
 import com.espertech.esper.client.UpdateListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import pl.cyfronet.dice.sad.cep.Engine;
-import pl.cyfronet.dice.sad.cep.EventDefinitionsManager;
-import pl.cyfronet.dice.sad.cep.RedisComplexEventSink;
+import pl.cyfronet.dice.sad.cep.*;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisPubSub;
 
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Phaser;
 
 /**
  * Created by tomek on 04.02.15.
@@ -18,37 +21,89 @@ public class SuboptimalAllocationDetector {
 
     private static final Logger log = LoggerFactory.getLogger(SuboptimalAllocationDetector.class);
 
+    private final Phaser phaser = new Phaser();
     private final Configuration config;
+    private JedisPool jedisPool;
     private UpdateListener complexEvListener;
     private EventDefinitionsManager evDefMng;
+    private JedisPubSub evDefSub;
+    private SimpleEventSub simpleEventSub;
 
-    public SuboptimalAllocationDetector() {
+    public SuboptimalAllocationDetector() throws SADException {
         config = Configuration.fromPropertiesFile();
-        try {
-            evDefMng = new EventDefinitionsManager();
-            complexEvListener = RedisComplexEventSink.getInstance();
-        } catch (SADException e) {
-            log.error(
-                    "Failed to start Suboptimal Allocation Detector:\n\t"
-                    + e.getMessage()
-            );
-        }
-
+        evDefSub = EventDefinitionsSub.getInstance();
+        simpleEventSub = new SimpleEventSub();
+        evDefMng = new EventDefinitionsManager();
+        complexEvListener = RedisComplexEventSink.getInstance();
     }
 
-    public static void main(String[] args) throws InterruptedException, URISyntaxException {
+    public static void main(String[] args) {
         log.info("Starting Suboptimal Allocation Detector");
-
-        new SuboptimalAllocationDetector().start();
+        SuboptimalAllocationDetector sad = null;
+        try {
+            sad = new SuboptimalAllocationDetector();
+        } catch (SADException se) {
+            log.error(
+                    "Failed to create SAD application:" + se.getMessage()
+            );
+            System.exit(1);
+        }
+        sad.start();
     }
 
     private void start() {
-        evDefMng.start();
-
+        setupJedisPool();
+        setupEvDefinitionsListener();
+        setupSimpleEvListener();
     }
 
+    private void setupJedisPool() {
+        jedisPool = new JedisPool(
+                new JedisPoolConfig(),
+                config.getRedisHost(),
+                config.getRedisPort(),
+                config.getRedisTimeout(),
+                config.getRedisPassword()
+        );
+        Runtime.getRuntime().addShutdownHook(
+                new Thread(
+                        () -> {
+                            phaser.register();
+                            evDefSub.unsubscribe();
+                            simpleEventSub.unsubscribe();
+                            phaser.arriveAndAwaitAdvance();
+                            if (jedisPool != null) {
+                                jedisPool.destroy();
+                                log.debug("Destroyed Jedis pool");
+                            }
+                        }
+                )
+        );
+    }
 
+    private void setupEvDefinitionsListener() {
+        new Thread(() -> {
+            phaser.register();
+            Jedis jedis = jedisPool.getResource();
+            jedis.subscribe(evDefSub, config.getEventDefinitionsChannelName());
+            log.debug("Releasing Jedis resource from EventDefinitionListener");
+            jedisPool.returnResource(jedis);
+            phaser.arrive();
+        }).start();
+        log.info("Subscribed for event definitions");
+    }
 
+    private void setupSimpleEvListener() {
+        new Thread(() -> {
+            phaser.register();
+            Jedis jedis = jedisPool.getResource();
+            jedis.subscribe(simpleEventSub, config.getEventDefinitionsChannelName());
+            log.debug("Releasing Jedis resource from SimpleEventListener");
+            jedisPool.returnResource(jedis);
+            phaser.arrive();
+        }).start();
+        log.info("Subscribed for simple events");
+    }
 
     //========================================================================================
 
--- src/main/java/pl/cyfronet/dice/sad/cep/EventDefinitionsManager.java
+++ src/main/java/pl/cyfronet/dice/sad/cep/EventDefinitionsManager.java
@@ -2,11 +2,7 @@ package pl.cyfronet.dice.sad.cep;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import pl.cyfronet.dice.sad.Configurator;
 import pl.cyfronet.dice.sad.SADException;
-import redis.clients.jedis.*;
-
-import java.util.concurrent.Phaser;
 
 /**
  * Created by tomek on 8/3/15.
@@ -15,73 +11,15 @@ public class EventDefinitionsManager {
 
     private static final Logger log = LoggerFactory.getLogger(EventDefinitionsManager.class);
 
-    private JedisPool jedisPool;
-    private String evDefChannel;
-    private String simpleEventChannel;
-    private JedisPubSub evDefSub;
-    private SimpleEventSub simpleEventSub;
-
-    private final Phaser phaser = new Phaser();
-
     public EventDefinitionsManager() throws SADException {
-        Configurator configurator = Configurator.INSTANCE;
-        evDefChannel = configurator.getProperty("eventDefinitionChannel");
-        evDefSub = EventDefinitionsSub.getInstance();
-        simpleEventChannel = configurator.getProperty("simpleEventChannel");
-        simpleEventSub = new SimpleEventSub();
+
     }
 
-    public void start() {
-        setupJedisPool();
-        setupEvDefinitionsListener();
-        setupSimpleEvListener();
-    }
 
-    private void setupSimpleEvListener() {
-        new Thread(() -> {
-            phaser.register();
-            Jedis jedis = jedisPool.getResource();
-            jedis.subscribe(simpleEventSub, simpleEventChannel);
-            log.debug("Releasing Jedis resource from SimpleEventListener");
-            jedisPool.returnResource(jedis);
-            phaser.arrive();
-        }).start();
-        log.info("Subscribed for simple events");
-    }
 
-    private void setupEvDefinitionsListener() {
-        new Thread(() -> {
-            phaser.register();
-            Jedis jedis = jedisPool.getResource();
-            jedis.subscribe(evDefSub, evDefChannel);
-            log.debug("Releasing Jedis resource from EventDefinitionListener");
-            jedisPool.returnResource(jedis);
-            phaser.arrive();
-        }).start();
-        log.info("Subscribed for event definitions");
-    }
 
-    private void setupJedisPool() {
-        Configurator configurator = Configurator.INSTANCE;
-        String redisHost = configurator.getProperty("redisHost");
-        int redisPort = Integer.parseInt(configurator.getProperty("redisPort"));
-        int redisTimeout = Integer.parseInt(configurator.getProperty("redisTimeout"));
-        String redisPassword = configurator.getProperty("redisPassword");
-        jedisPool = new JedisPool(new JedisPoolConfig(), redisHost, redisPort, redisTimeout, redisPassword);
-        Runtime.getRuntime().addShutdownHook(
-                new Thread(
-                        () -> {
-                            phaser.register();
-                            evDefSub.unsubscribe();
-                            simpleEventSub.unsubscribe();
-                            phaser.arriveAndAwaitAdvance();
-                            if (jedisPool != null) {
-                                jedisPool.destroy();
-                                log.debug("Destroyed Jedis pool");
-                            }
-                        }
-                )
-        );
-    }
+
+
+
 
 }
